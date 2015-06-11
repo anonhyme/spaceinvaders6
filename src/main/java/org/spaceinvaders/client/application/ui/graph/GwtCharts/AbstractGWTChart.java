48ea95f91c69250ed26400bf51127fdef5fe64ef
Nerrull <et.richan@gmail.com>
2015-06-11 10:42:05 -0400

    Added size function to abstractGWTChart

--- src/main/java/org/spaceinvaders/client/application/ui/graph/GwtCharts/AbstractGWTChart.java
+++ src/main/java/org/spaceinvaders/client/application/ui/graph/GwtCharts/AbstractGWTChart.java
@@ -2,6 +2,7 @@ package org.spaceinvaders.client.application.ui.graph.GwtCharts;
 
 import com.google.gwt.user.client.ui.Widget;
 import com.googlecode.gwt.charts.client.corechart.ColumnChart;
+import com.googlecode.gwt.charts.client.corechart.CoreChartWidget;
 import org.spaceinvaders.shared.dto.CompetenceEvalResult;
 
 import java.util.List;
@@ -10,7 +11,11 @@ import java.util.List;
  * Created by Etienne on 2015-06-10.
  */
 public abstract class AbstractGWTChart{
+
     private List<CompetenceEvalResult> chartData;
+    private int width;
+    private int height;
+    public boolean IsCustomSize = false;
 
     abstract public Widget getChart();
     public void setChartData(List<CompetenceEvalResult> data){
@@ -22,5 +27,11 @@ public abstract class AbstractGWTChart{
         return chartData;
     }
     abstract public void loadChart();
+
+    public void setSize(int width, int height){
+        this.width = width;
+        this.height = height;
+        IsCustomSize = true;
+    }
 
 }
