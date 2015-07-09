4a3c6a484dfe548f7b48d9cd2a7955c7729c5099
anonhyme <antoine.bouchard2@usherbrooke.ca>
2015-07-08 23:58:33 -0400

    clean popuppresenter

--- src/main/java/org/spaceinvaders/client/application/ApplicationModule.java
+++ src/main/java/org/spaceinvaders/client/application/ApplicationModule.java
@@ -1,13 +1,13 @@
 package org.spaceinvaders.client.application;
 
 import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
+
 import org.spaceinvaders.client.application.ap.ApModule;
 import org.spaceinvaders.client.application.error.ErrorModule;
 import org.spaceinvaders.client.application.semester.SemesterModule;
 import org.spaceinvaders.client.application.widgets.graph.gwtchartswidget.GwtChartWidgetModule;
 import org.spaceinvaders.client.application.widgets.grid.GridModule;
 import org.spaceinvaders.client.application.widgets.menu.MenuModule;
-import org.spaceinvaders.client.application.widgets.mypopup.MyPopupModule;
 
 public class ApplicationModule extends AbstractPresenterModule {
     @Override
@@ -22,6 +22,5 @@ public class ApplicationModule extends AbstractPresenterModule {
         install(new ApModule());
         install(new ErrorModule());
         install(new GridModule());
-        install(new MyPopupModule());
     }
 }
--- src/main/java/org/spaceinvaders/client/application/widgets/mypopup/MyPopupUiHandlers.java
+++
@@ -1,6 +0,0 @@
-package org.spaceinvaders.client.application.widgets.mypopup;
-
-import com.gwtplatform.mvp.client.UiHandlers;
-
-public interface MyPopupUiHandlers extends UiHandlers {
-}
