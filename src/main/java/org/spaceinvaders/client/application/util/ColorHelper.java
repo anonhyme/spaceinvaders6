1107e389833a1a3765f10a39c6f58684c2290dd8
anonhyme <antoine.bouchard2@usherbrooke.ca>
2015-07-13 16:11:25 -0400

    Revert "color update"
    This reverts commit 62728c8179b1227ce8febd6cefec9f72081c5e80.

--- src/main/java/org/spaceinvaders/client/application/util/ColorHelper.java
+++ src/main/java/org/spaceinvaders/client/application/util/ColorHelper.java
@@ -20,11 +20,11 @@ public final class ColorHelper {
     public static final String GREEN_UDES = "#2d9d5f";
 
     public static final String GOLD = "#FFD700";
-    public static final String _blue = "#049cdb";
-    public static final String _green = "#46a546";
-    public static final String _red = "#9d261d";
-    public static final String _yellow = "#ffc40d";
-    public static final String _orange = "#f89406";
-    public static final String _pink = "#c3325f";
-    public static final String _purple = "#7a43b6";
+    public static final _blue = #049cdb	
+	public static final _green = #46a546	
+	public static final _red = #9d261d	
+	public static final _yellow = #ffc40d	
+	public static final _orange = #f89406	
+	public static final _pink = #c3325f	
+	public static final _purple = #7a43b6
 }
